4dad726c69d8cc2ecf6426b34bd1f2065d489bbd
CapronLee <kasamata2.2003@gmail.com>
2024-09-16 08:09:16 +0700

    add route

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,20 +1,20 @@
 // src/App.js
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CartPage from './pages/Cart/CartPage';
 import CheckoutPage from './pages/CheckOut/CheckoutPage';
+import HomePage from './pages/Home/HomePage';
 import NotFoundPage from './pages/NotFound/NotFoundPage';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
